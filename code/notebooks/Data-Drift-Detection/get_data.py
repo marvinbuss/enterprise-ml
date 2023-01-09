2a2a0f5c15e7af66fb8e8831dd426edc40c465a0
Marvin Buss <marvin.buss@gmail.com>
2023-01-09 11:48:04 +0100

    lint

--- code/notebooks/Data-Drift-Detection/get_data.py
+++ code/notebooks/Data-Drift-Detection/get_data.py
@@ -1,8 +1,9 @@
 # import packages
 import os
-import pandas as pd
 from calendar import monthrange
 from datetime import datetime, timedelta
+
+import pandas as pd
 from azureml.core import Dataset, Datastore, Workspace
 from azureml.opendatasets import NoaaIsdWeather
 
@@ -17,14 +18,14 @@ start_month = 1
 # get data
 for year in target_years:
     for month in range(start_month, 12 + 1):
-        path = 'weather-data/{}/{:02d}/'.format(year, month)
+        path = "weather-data/{}/{:02d}/".format(year, month)
         try:
             start = datetime(year, month, 1)
             end = datetime(year, month, monthrange(year, month)[1]) + timedelta(days=1)
             isd = NoaaIsdWeather(start, end).to_pandas_dataframe()
-            isd = isd[isd['stationName'].str.contains('FLORIDA', regex=True, na=False)]
+            isd = isd[isd["stationName"].str.contains("FLORIDA", regex=True, na=False)]
             os.makedirs(path, exist_ok=True)
-            isd.to_parquet(path + 'data.parquet')
+            isd.to_parquet(path + "data.parquet")
         except Exception as e:
-            print('Month {} in year {} likely has no data.\n'.format(month, year))
-            print('Exception: {}'.format(e))
+            print("Month {} in year {} likely has no data.\n".format(month, year))
+            print("Exception: {}".format(e))
